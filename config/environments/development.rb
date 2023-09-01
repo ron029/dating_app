79d34b99ddf0fc84fe8c749b1d9f04034ce8bc18
ron029 <bonifacioronmark@gmail.com>
2023-09-02 01:24:00 +0800

    Fix graphiql error in production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,8 +62,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.assets.paths << Rails.root.join('node_modules')
-
+  config.assets.precompile += ['graphiql/rails/application.js', 'graphiql/rails/application.css']
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 end
